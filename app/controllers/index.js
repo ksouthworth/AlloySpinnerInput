72e68860312231f9f2086e1dbd71f77b5dac7955
Kevin Southworth <kevin.southworth@webascender.com>
2015-01-15 15:26:30 -0500

    switch to on-screen spinners below input, added 'value' get/set

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,4 +11,8 @@ function doClick(e) {
     alert($.label.text);
 }
 
+function getValue() {
+    alert('Spinner 1 = ' + $.spin1.value);
+}
+
 $.index.open();
--- app/widgets/com.ksouthworth.spinnerinput/controllers/widget.js
+++ app/widgets/com.ksouthworth.spinnerinput/controllers/widget.js
@@ -1,15 +1,13 @@
-var AUTO_CLOSE_MILLIS = 500;
-var STYLES = {
-  controlsContainer: $.createStyle({classes: 'controlsContainer'}),
-  bigSpinner: $.createStyle({classes: 'bigSpinner'}),
-  topSpinner: $.createStyle({classes: 'bigSpinner topSpinner'}),
-  bottomSpinner: $.createStyle({classes: 'bigSpinner bottomSpinner'}),
-  spinnerWindow: $.createStyle({classes: 'spinnerWindow'}),
-  spinnerSeparator: $.createStyle({classes: 'spinnerSeparator'})
-};
-var _absolutePosition = null;
-var _spinnerLastTouchedAt = null;
-var _textFieldHasFocus = false;
+// var AUTO_CLOSE_MILLIS = 500;
+// var STYLES = {
+//   controlsContainer: $.createStyle({classes: 'controlsContainer'}),
+//   bigSpinner: $.createStyle({classes: 'bigSpinner'}),
+//   topSpinner: $.createStyle({classes: 'bigSpinner topSpinner'}),
+//   bottomSpinner: $.createStyle({classes: 'bigSpinner bottomSpinner'}),
+//   spinnerWindow: $.createStyle({classes: 'spinnerWindow'}),
+//   spinnerSeparator: $.createStyle({classes: 'spinnerSeparator'})
+// };
+
 var _spinnerStepValue = 1;
 
 //-----------------------------
@@ -22,7 +20,6 @@ if(arguments[0]) {
     exports.id = args.id;
     delete args.id;
   }
-  //delete args.__parentSymbol;
   delete args.__itemTemplate;
   delete args['$model'];
   delete args.children;
@@ -37,65 +34,6 @@ if(arguments[0]) {
   }
 }
 
-//-----------------------------
-// Create Spinner Window
-//-----------------------------
-var _spinnerWindow = Ti.UI.createWindow({
-  width: Ti.UI.FILL,
-  height: Ti.UI.FILL,
-  //backgroundColor: 'yellow',
-  //opacity: 0.1
-  backgroundColor: 'transparent'
-});
-var overlay = Ti.UI.createView({
-  width: Ti.UI.FILL,
-  height: Ti.UI.FILL,
-  backgroundColor: 'black',
-  opacity: 0.4
-});
-
-var controlsContainer = Ti.UI.createView(STYLES.controlsContainer);
-var _spinnerUp = Ti.UI.createLabel(_.extend(STYLES.topSpinner, {
-  text: '\uf077',
-  width: '50dp'
-}));
-var _spinnerDown = Ti.UI.createLabel(_.extend(STYLES.bottomSpinner, {
-  text: '\uf078',
-  width: '50dp'
-}));
-_spinnerUp.addEventListener('touchstart', function(e) { e.spinDirection = 'up'; spinnerClick(e); });
-_spinnerDown.addEventListener('touchstart', function(e) { e.spinDirection = 'down'; spinnerClick(e); });
-
-controlsContainer.add(_spinnerUp);
-controlsContainer.add(Ti.UI.createView(STYLES.spinnerSeparator));
-controlsContainer.add(_spinnerDown);
-_spinnerWindow.add(overlay);
-_spinnerWindow.add(controlsContainer);
-_spinnerWindow.addEventListener('touchstart', onSpinnerWindowTouch);
-
-/*
-var _spinnerUp = Ti.UI.createLabel(_.extend(STYLES.bigSpinner, {
-  text: '\uf077'
-}));
-var _spinnerDown = Ti.UI.createLabel(_.extend(STYLES.bigSpinner, {
-  text: '\uf078'
-}));
-_spinnerUp.addEventListener('touchstart', function(e) { e.spinDirection = 'up'; bigSpinnerClick(e); });
-_spinnerDown.addEventListener('touchstart', function(e) { e.spinDirection = 'down'; bigSpinnerClick(e); });
-
-var _spinnerWindow = Ti.UI.createWindow(_.extend(STYLES.spinnerWindow, {
-  width: '50dp',
-  height: '100dp',
-  layout: 'vertical'
-}));
-_spinnerWindow.add(_spinnerUp);
-_spinnerWindow.add(Ti.UI.createView(STYLES.spinnerSeparator));
-_spinnerWindow.add(_spinnerDown);
-_spinnerWindow.addEventListener('blur', function() {
-  Ti.API.debug('spinner ' + exports.id + ' -> window [blur]');
-  _spinnerWindow.close();
-});
-*/
 
 /**
  * Apply TSS style properties to our widget
@@ -106,7 +44,7 @@ _spinnerWindow.addEventListener('blur', function() {
 function applyProperties(properties) {
   properties = _.omit(properties, 'children');
 
-  Ti.API.debug('spinner ' + exports.id + ' -> applyProperties : ' + JSON.stringify(properties));
+  // Ti.API.debug('spinner ' + exports.id + ' -> applyProperties : ' + JSON.stringify(properties));
 
   $.view.applyProperties(properties);
 
@@ -118,146 +56,27 @@ function applyProperties(properties) {
   }
 }
 
-function onPostLayout() {
-  Ti.API.debug('spinner ' + exports.id + ' -> [postlayout]');
-
-  if(_absolutePosition === null) {
-    calculateAbsolutePosition();
-  }
-}
-
-function calculateAbsolutePosition() {
-  var pos = {
-    x: $.view.rect.x,
-    y: $.view.rect.y
-  };
-
-  Ti.API.debug('calculateAbsolutePosition -> start = ' + JSON.stringify(pos));
-
-  var parent = $.parent;
-  // Ti.API.debug('calculateAbsolutePosition -> parent = ' + parent);
-  while(parent) {
-    // Ti.API.debug('calculateAbsolutePosition -> parent = ' + parent);
-
-    pos.x = pos.x + parent.rect.x;
-    pos.y = pos.y + parent.rect.y;
-
-    parent = parent.parent;
-  }
-
-  Ti.API.debug('calculateAbsolutePosition -> end = ' + JSON.stringify(pos));
-
-  _absolutePosition = pos;
-
-  controlsContainer.applyProperties({
-    top: (_absolutePosition ? _absolutePosition.y : $.view.rect.y) - 50 + 18,
-    left: (_absolutePosition ? _absolutePosition.x : $.view.rect.x) + $.textField.rect.width,
-  });
-}
-
-function onTextFocus(e) {
-  Ti.API.debug('spinner ' + exports.id + ' -> [textFocus]');
-}
-
-function onTextBlur(e) {
-  Ti.API.debug('spinner ' + exports.id + ' -> [textBlur]');
-}
-
 function onKeyboardDone(e) {
-  Ti.API.debug('spinner ' + exports.id + ' -> [keyboard done]');
-  _spinnerWindow.close();
+  //Ti.API.debug('spinner ' + exports.id + ' -> [keyboard done]');
+
   $.textField.blur();
 }
 
-function spinnerClick(e) {
-  e.cancelBubble = true;
-  Ti.API.debug('spinner ' + exports.id + ' -> ' + e.spinDirection);
-  // _spinnerLastTouchedAt = new Date();
-  // if(e.spinDirection == 'up') {
-  //   spinUp();
-  // } else if(e.spinDirection == 'down') {
-  //   spinDown();
-  // }
-}
-
-function onSpinnerWindowTouch(e) {
-  Ti.API.debug('spinner window ' + exports.id + ' -> [touch]');
-  _spinnerWindow.close();
-}
-
-function onShowSpinner(e) {
-  //_spinnerWindow.open({modal: true});
-
-  // controlsContainer.applyProperties({
-  //   top: (_absolutePosition ? _absolutePosition.y : $.view.rect.y) - 50 + 18,
-  //   left: (_absolutePosition ? _absolutePosition.x : $.view.rect.x) + $.textField.rect.width,
-  // });
-  _spinnerWindow.open();
-}
-
-/*
-function onTextFocus(e) {
-  Ti.API.debug('spinner ' + exports.id + ' -> [textFocus]');
-  _textFieldHasFocus = true;
-  _spinnerWindow.close();
-}
-
-function onTextBlur(e) {
-  Ti.API.debug('spinner ' + exports.id + ' -> [textBlur]');
-  _spinnerWindow.close();
-}
-
-function setValue(value) {
-  $.textField.value = value;
-}
-
-function spinUp(e) {
+function onSpinUp(e) {
   setValue(parseFloat($.textField.value) + _spinnerStepValue);
 }
 
-function spinDown(e) {
+function onSpinDown(e) {
   setValue(parseFloat($.textField.value) - _spinnerStepValue);
 }
 
-function onDone(e) {
-  dismissKeyboard();
+function getValue(value) {
+  return $.textField.value;
 }
-
-function dismissKeyboard(e) {
-  $.textField.blur();
-  _textFieldHasFocus = false;
-  closeSpinnerWindow();
+function setValue(value) {
+  $.textField.value = value;
 }
-
-function gestureTouchStart(e) {
-  _spinnerWindow.open({
-    top: (_absolutePosition ? _absolutePosition.y : $.view.rect.y) - 50 + 18,
-    left: (_absolutePosition ? _absolutePosition.x : $.view.rect.x) + $.textField.rect.width,
-  });
-
-  setTimeout(function() {
-    closeSpinnerWindow();
-  }, 2000);
-}
-
-function bigSpinnerClick(e) {
-  _spinnerLastTouchedAt = new Date();
-  if(e.spinDirection == 'up') {
-    spinUp();
-  } else if(e.spinDirection == 'down') {
-    spinDown();
-  }
-}
-
-function closeSpinnerWindow() {
-  var now = new Date();
-
-  var closeThresholdElapsed = ((now - _spinnerLastTouchedAt) >= AUTO_CLOSE_MILLIS);
-  if(!_textFieldHasFocus && closeThresholdElapsed) {
-    _spinnerWindow.close();
-  } else {
-    // check again later
-    setTimeout(closeSpinnerWindow, 100);
-  }
-}
-*/
+Object.defineProperty($, "value", {
+    get: getValue,
+    set: setValue
+});
